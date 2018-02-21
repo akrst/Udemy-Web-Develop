d8c769bc213094c0ef872598478b5f4077dba715
akrst <akirasato@psychic-vr-lab.com>
2018-02-21 23:06:05 +0900

    My Fisrst php Project started.

---
+++ PHP/00hello.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hi this is my frist PHP file</h1>
+
+	code output;
+
+	<?php
+		// Script PHP version
+		// Created in February 21, 2018
+		// By Akira Sato
+		// This Script is sample
+
+		echo "Hello World!";
+
+		echo "Sample　Script";
+
+
+	 ?>
+
+</body>
+</html>
---
+++ PHP/07variables&scope.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hi this is my frist PHP file</h1>
+
+	code output;
+
+	<?php
+		// Boolean (TRUE or FALSE)
+		// Interger (numbers)
+		// Floating Point(decimals)
+		// Strings (text characters)
+		// arrays and objects
+		// NULL
+
+		$name = "victor";
+
+		echo "hi my name is ".$name."!";
+	 ?>
+
+</body>
+</html>
